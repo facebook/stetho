705d78877c717755cbf87116d99b06052c7725ab
Josh Guilfoyle <jasta@devtcg.org>
2015-03-24 14:55:40 -0700

    Fix Copyright consistency issues.
    An accidental lack of enforcement of the copyright template left the
    source inconsistently marked.  The first step is to correct the
    formatting of existing files, then we will contribute the
    IntelliJ/Android Studio project files which enforce it in the future.

--- stetho/src/main/java/com/facebook/stetho/server/LocalSocketHttpServer.java
+++ stetho/src/main/java/com/facebook/stetho/server/LocalSocketHttpServer.java
@@ -1,32 +1,10 @@
-// Copyright 2004-present Facebook. All Rights Reserved.
-// This is based on ElementalHttpServer.java in the Apache httpcore
-// examples.
-
 /*
- * ====================================================================
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Copyright (c) 2014-present, Facebook, Inc.
+ * All rights reserved.
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
 
 package com.facebook.stetho.server;
