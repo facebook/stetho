10ccd5a91b9cd1c40d38560765dbc79d72a6f15e
Josh Guilfoyle <jasta@devtcg.org>
2015-05-01 00:21:48 -0700

    Fix copyright headers hopefully for the last time :)
    The IntelliJ copyright settings are now part of the repo and new
    developers should automagically import them to not cause this problem in
    the future.

--- stetho/src/main/java/com/facebook/stetho/websocket/Frame.java
+++ stetho/src/main/java/com/facebook/stetho/websocket/Frame.java
@@ -6,8 +6,6 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  */
-//
-// Copyright 2004-present Facebook. All Rights Reserved.
 
 package com.facebook.stetho.websocket;
 
