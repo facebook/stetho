3ca2ec1bb1d43c342a224728cb235920a117a076
Aman Talwar <aexyno@gmail.com>
2015-07-22 20:03:18 +0530

    Load External databases if Present

--- stetho/src/main/java/com/facebook/stetho/inspector/database/DefaultDatabaseFilesProvider.java
+++ stetho/src/main/java/com/facebook/stetho/inspector/database/DefaultDatabaseFilesProvider.java
@@ -10,9 +10,11 @@
 package com.facebook.stetho.inspector.database;
 
 import android.content.Context;
+import android.os.Environment;
+
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -32,6 +34,45 @@ public final class DefaultDatabaseFilesProvider implements DatabaseFilesProvider
     for (String filename : mContext.databaseList()) {
       databaseFiles.add(new File(filename));
     }
+
+    FilenameFilter filenameFilter = new FilenameFilter() {
+      @Override
+      public boolean accept(File dir, String filename) {
+        return filename.endsWith(".sql")
+            || filename.endsWith(".sqlite")
+            || filename.endsWith(".db");
+      }
+    };
+
+    //Add External Databases if Present
+    if(isExternalAvailable()){
+      final File absoluteFile = mContext.getExternalFilesDir(null).getAbsoluteFile();
+      String[] externalDatabases= absoluteFile.list(filenameFilter);
+      for (String db:externalDatabases){
+        databaseFiles.add(new File(absoluteFile,db));
+      }
+    }
     return databaseFiles;
+  }
+
+  /**
+   * @return true if External Storage Present
+   */
+  private static boolean isExternalAvailable() {
+    boolean mExternalStorageAvailable = false;
+    String state = Environment.getExternalStorageState();
+
+    if (Environment.MEDIA_MOUNTED.equals(state)) {
+      // We can read and write the media
+      mExternalStorageAvailable = true;
+    } else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+      // We can only read the media
+      mExternalStorageAvailable = true;
+    } else {
+      // Something else is wrong. It may be one of many other states, but all we need
+      //  to know is we can neither read nor write
+      mExternalStorageAvailable  = false;
+    }
+    return mExternalStorageAvailable;
   }
 }
