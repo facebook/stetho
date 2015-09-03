62c4a4874914675b75744dab22ff78ae3a7f7f79
Josh Guilfoyle <jasta@devtcg.org>
2015-09-03 10:56:59 -0700

    Fix the Rhino autodetection feature
    This was accidentally broken in #245 by not modifying the default
    inspector modules provider to use the new Runtime(Context) constructor,
    effectively disabling the entire feature.

--- stetho/src/main/java/com/facebook/stetho/Stetho.java
+++ stetho/src/main/java/com/facebook/stetho/Stetho.java
@@ -274,7 +274,7 @@ public class Stetho {
       provideIfDesired(new Network(mContext));
       provideIfDesired(new Page(mContext));
       provideIfDesired(new Profiler());
-      provideIfDesired(new Runtime());
+      provideIfDesired(new Runtime(mContext));
       provideIfDesired(new Worker());
       if (Build.VERSION.SDK_INT >= DatabaseConstants.MIN_API_LEVEL) {
         provideIfDesired(new Database(mContext, new DefaultDatabaseFilesProvider(mContext)));
