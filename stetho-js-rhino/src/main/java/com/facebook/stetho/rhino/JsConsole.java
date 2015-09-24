3c334e44d63d1409995f13a7efacdccecb2c5ee7
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2015-09-24 23:26:34 +0200

    Improved JSConsole when handling "%d" with a float as argument
    In JavaScript every number is a floating point number, even ints! This causes
    console.log("%d", 42) to fail because the initial implemenation was using
    String.format() and it can't handle a floating point with %d.
    
    We now have a custom formatter that mimics what google chrome does (with a few
    improvemnts).
    
    This patch also adds unit tests to the project stetho-js-rhino.

--- stetho-js-rhino/src/main/java/com/facebook/stetho/rhino/JsConsole.java
+++ stetho-js-rhino/src/main/java/com/facebook/stetho/rhino/JsConsole.java
@@ -55,27 +55,9 @@ public class JsConsole extends ScriptableObject {
     log(args);
   }
 
+  // See https://developer.chrome.com/devtools/docs/console-api#consolelogobject-object
   private static void log(Object [] rawArgs) {
-    String format = (String) rawArgs[0];
-
-    String message;
-    if (rawArgs.length == 1) {
-      message = format;
-    }
-    else {
-      // Using place holders in javascript (%d) causes a problem with java's String.format().
-      // This happens because %d expects an int/Integer but in javascript numbers are floats.
-      // For now as a best effort we just try to use jsToJava() to convert everything to objects.
-      // This will need rework.
-      Object [] args = new Object[rawArgs.length - 1];
-      for (int i = 0; i < args.length; ++i) {
-        Object arg = rawArgs[i + 1];
-        arg = Context.jsToJava(arg, Object.class);
-        args[i] = arg;
-      }
-      message = String.format(format, args);
-    }
-
+    String message = JsFormat.parse(rawArgs);
     CLog.writeToConsole(MessageLevel.LOG, MessageSource.JAVASCRIPT, message);
   }
 }
---
+++ stetho-js-rhino/src/test/java/com/facebook/stetho/rhino/JsFormatTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2014-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.stetho.rhino;
+
+import android.os.Build;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.*;
+
+@Config(emulateSdk = Build.VERSION_CODES.JELLY_BEAN)
+@RunWith(RobolectricTestRunner.class)
+public class JsFormatTest {
+
+  @Test
+  public void testParse() {
+    assertEquals("%d", JsFormat.parse("%d"));
+    assertEquals("23", JsFormat.parse("%d", 23));
+    assertEquals("2", JsFormat.parse("%d", 2.7));
+    assertEquals("23.37", JsFormat.parse("%.2f", 23.3678));
+    assertEquals("hellow world", JsFormat.parse("hellow %s", "world"));
+    assertEquals("%$2d dp 5 1 2 3 4", JsFormat.parse("%$2d dp 5", 1, 2, 3 ,4));
+    assertEquals("2 dp 4 1 3 4", JsFormat.parse("%2$d dp 4", 1, 2, 3 ,4));
+    assertEquals("NaN dp 4 one three four", JsFormat.parse("%2$d dp 4", "one", "two", "three", "four"));
+    assertEquals("two dp 4 one three four", JsFormat.parse("%2$s dp 4", "one", "two", "three", "four"));
+    assertEquals("two vs two one three four", JsFormat.parse("%2$s vs %2$s", "one", "two", "three", "four"));
+
+    assertEquals("two vs two one three four", JsFormat.parse("%2$s vs %2$s", "one", "two", "three", "four"));
+    assertEquals("two vs four one three", JsFormat.parse("%2$s vs %4$s", "one", "two", "three", "four"));
+    assertEquals("two vs four %.3$4f one three 1234.5678", JsFormat.parse("%2$s vs %4$s %.3$4f", "one", "two", "three", "four", 1234.5678));
+    assertEquals("two vs four %.2$4f one three 1234.5678", JsFormat.parse("%2$s vs %4$s %.2$4f", "one", "two", "three", "four", 1234.5678));
+    assertEquals("two vs four 1234.57 one three", JsFormat.parse("%2$s vs %4$s %.2f", "one", "two", "three", "four", 1234.5678));
+
+    assertEquals("1234.57", JsFormat.parse("%.2f", 1234.5678));
+    assertEquals("%,2f 1234.5678", JsFormat.parse("%,2f", 1234.5678));
+    assertEquals("1234.57", JsFormat.parse("%.2f", 1234.5678));
+    assertEquals("NaN", JsFormat.parse("%.2f", "hello"));
+    assertEquals("%.2a hello", JsFormat.parse("%.2a", "hello"));
+    assertEquals("% cool .2a hello", JsFormat.parse("%% cool .2a", "hello"));
+    assertEquals("two vs four 1234.5678 one three", JsFormat.parse("%2$s vs %4$s %s", "one", "two", "three", "four", 1234.5678));
+    assertEquals("two vs four %s", JsFormat.parse("two vs four %s"));
+    assertEquals("two vs four % one three", JsFormat.parse("two vs four %", "one", "three"));
+    assertEquals("two one two four oops three", JsFormat.parse("%2$s %1$s %s %4$s %s", "one", "two", "three", "four", "oops"));
+  }
+}
