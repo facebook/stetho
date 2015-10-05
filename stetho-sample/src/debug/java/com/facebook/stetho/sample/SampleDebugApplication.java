18dbfc9664d3209e6394b2c709b1b9a0f9ab54a2
Roman Kushnarenko <sromku@gmail.com>
2015-10-05 19:42:00 +0300

    Refined content provider initialization based on better api.

--- stetho-sample/src/debug/java/com/facebook/stetho/sample/SampleDebugApplication.java
+++ stetho-sample/src/debug/java/com/facebook/stetho/sample/SampleDebugApplication.java
@@ -22,11 +22,7 @@ import com.facebook.stetho.dumpapp.DumperPlugin;
 import com.facebook.stetho.inspector.database.ContentProviderDatabaseDriver;
 import com.facebook.stetho.inspector.database.ContentProviderSchema;
 import com.facebook.stetho.inspector.database.ContentProviderSchema.Table;
-import com.facebook.stetho.inspector.database.DefaultDatabaseFilesProvider;
-import com.facebook.stetho.inspector.database.SqliteDatabaseDriver;
 import com.facebook.stetho.inspector.protocol.ChromeDevtoolsDomain;
-import com.facebook.stetho.inspector.protocol.module.Database;
-import com.facebook.stetho.inspector.protocol.module.DatabaseConstants;
 
 public class SampleDebugApplication extends SampleApplication {
   private static final String TAG = "SampleDebugApplication";
@@ -65,47 +61,43 @@ public class SampleDebugApplication extends SampleApplication {
       mContext = context;
     }
 
-    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     @Override
     public Iterable<ChromeDevtoolsDomain> get() {
-      Stetho.DefaultInspectorModulesBuilder builder = new Stetho.DefaultInspectorModulesBuilder(mContext);
-      if (Build.VERSION.SDK_INT >= DatabaseConstants.MIN_API_LEVEL) {
+      return new Stetho.DefaultInspectorModulesBuilder(mContext)
+          .provideDatabaseDriver(createContentProviderDatabaseDriver(mContext))
+          .finish();
+    }
 
-        // sample calendars content provider we want to support
-        ContentProviderSchema calendarsSchema = new ContentProviderSchema.Builder()
-            .table(new Table.Builder()
-                .uri(CalendarContract.Calendars.CONTENT_URI)
-                .projection(new String[] {
-                    CalendarContract.Calendars._ID,
-                    CalendarContract.Calendars.NAME,
-                    CalendarContract.Calendars.ACCOUNT_NAME,
-                    CalendarContract.Calendars.IS_PRIMARY,
-                })
-                .build())
-            .build();
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    private ContentProviderDatabaseDriver createContentProviderDatabaseDriver(Context context) {
+      ContentProviderSchema calendarsSchema = new ContentProviderSchema.Builder()
+          .table(new Table.Builder()
+              .uri(CalendarContract.Calendars.CONTENT_URI)
+              .projection(new String[] {
+                  CalendarContract.Calendars._ID,
+                  CalendarContract.Calendars.NAME,
+                  CalendarContract.Calendars.ACCOUNT_NAME,
+                  CalendarContract.Calendars.IS_PRIMARY,
+              })
+              .build())
+          .build();
 
-        // sample events content provider we want to support
-        ContentProviderSchema eventsSchema = new ContentProviderSchema.Builder()
-            .table(new Table.Builder()
-                .uri(CalendarContract.Events.CONTENT_URI)
-                .projection(new String[]{
-                    CalendarContract.Events._ID,
-                    CalendarContract.Events.TITLE,
-                    CalendarContract.Events.DESCRIPTION,
-                    CalendarContract.Events.ACCOUNT_NAME,
-                    CalendarContract.Events.DTSTART,
-                    CalendarContract.Events.DTEND,
-                    CalendarContract.Events.CALENDAR_ID,
-                })
-                .build())
-            .build();
-
-        Database database = new Database();
-        database.add(new SqliteDatabaseDriver(mContext, new DefaultDatabaseFilesProvider(mContext)));
-        database.add(new ContentProviderDatabaseDriver(mContext, calendarsSchema, eventsSchema));
-        builder.provide(database);
-      }
-      return builder.finish();
+      // sample events content provider we want to support
+      ContentProviderSchema eventsSchema = new ContentProviderSchema.Builder()
+          .table(new Table.Builder()
+              .uri(CalendarContract.Events.CONTENT_URI)
+              .projection(new String[]{
+                  CalendarContract.Events._ID,
+                  CalendarContract.Events.TITLE,
+                  CalendarContract.Events.DESCRIPTION,
+                  CalendarContract.Events.ACCOUNT_NAME,
+                  CalendarContract.Events.DTSTART,
+                  CalendarContract.Events.DTEND,
+                  CalendarContract.Events.CALENDAR_ID,
+              })
+              .build())
+          .build();
+      return new ContentProviderDatabaseDriver(context, calendarsSchema, eventsSchema);
     }
   }
 
