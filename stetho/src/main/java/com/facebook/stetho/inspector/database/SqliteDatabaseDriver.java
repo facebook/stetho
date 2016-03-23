e2d9a92f5cef617d191b7cc40df37cae755ad6bb
Daniel Frett <daniel.frett@ccci.org>
2016-03-23 17:50:06 -0400

    create a new constructor that takes a custom DatabaseConnectionProvider

--- stetho/src/main/java/com/facebook/stetho/inspector/database/SqliteDatabaseDriver.java
+++ stetho/src/main/java/com/facebook/stetho/inspector/database/SqliteDatabaseDriver.java
@@ -58,11 +58,25 @@ public class SqliteDatabaseDriver extends Database.DatabaseDriver {
   /**
    * @param context the context
    * @param databaseFilesProvider a database file name provider
+   * @deprecated use the other {@linkplain SqliteDatabaseDriver#SqliteDatabaseDriver(Context,
+   * DatabaseFilesProvider, DatabaseConnectionProvider) constructor} and pass in the {@linkplain
+   * DefaultDatabaseConnectionProvider default provider} .
    */
+  @Deprecated
   public SqliteDatabaseDriver(Context context, DatabaseFilesProvider databaseFilesProvider) {
+    this(context, databaseFilesProvider, new DefaultDatabaseConnectionProvider(context));
+  }
+
+  /**
+   * @param context the context
+   * @param databaseFilesProvider a database file name provider
+   * @param databaseConnectionProvider a database connection provider
+   */
+  public SqliteDatabaseDriver(Context context, DatabaseFilesProvider databaseFilesProvider,
+                              DatabaseConnectionProvider databaseConnectionProvider) {
     super(context);
     mDatabaseFilesProvider = databaseFilesProvider;
-    mDatabaseConnectionProvider = new DefaultDatabaseConnectionProvider(context);
+    mDatabaseConnectionProvider = databaseConnectionProvider;
   }
 
   @Override
