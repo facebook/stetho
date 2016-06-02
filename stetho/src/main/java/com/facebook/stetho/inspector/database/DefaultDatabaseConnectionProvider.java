f6e0bd2376cce3cf1b2dcdd725a2e04ae850a710
jasta <jasta@devtcg.org>
2016-06-01 17:45:14 -0700

    Improve support for custom SQLite open flags
    This diff adds a WAL-guessing heuristic to support most database users
    for the common case as well as expands upon
    DefaultDatabaseConnectionProvider to fully support subclassing to
    conveniently select custom options on a per database file basis.

--- stetho/src/main/java/com/facebook/stetho/inspector/database/DefaultDatabaseConnectionProvider.java
+++ stetho/src/main/java/com/facebook/stetho/inspector/database/DefaultDatabaseConnectionProvider.java
@@ -12,11 +12,16 @@ package com.facebook.stetho.inspector.database;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 
+import com.facebook.stetho.inspector.database.SQLiteDatabaseCompat.SQLiteOpenOptions;
+
 import java.io.File;
 
 /**
  * Opens the requested database using
  * {@link SQLiteDatabase#openDatabase(String, SQLiteDatabase.CursorFactory, int)} directly.
+ *
+ * <p>It is intended that this class be subclassed to enable/disable features via
+ * {@link #determineOpenOptions(File)}</p>
  */
 public class DefaultDatabaseConnectionProvider implements DatabaseConnectionProvider {
   public DefaultDatabaseConnectionProvider() {
@@ -24,10 +29,43 @@ public class DefaultDatabaseConnectionProvider implements DatabaseConnectionProv
 
   @Override
   public SQLiteDatabase openDatabase(File databaseFile) throws SQLiteException {
-    // Expected to throw if it cannot open the file (for example, if it doesn't exist).
-    return SQLiteDatabase.openDatabase(
+    return performOpen(
+        databaseFile,
+        determineOpenOptions(databaseFile));
+  }
+
+  /**
+   * Subclassing this function is intended to provide custom open behaviour on a per-file basis.
+   */
+  protected @SQLiteOpenOptions int determineOpenOptions(File databaseFile) {
+    @SQLiteOpenOptions int flags = 0;
+
+    // Try to guess if we should be using write-ahead logging.  If this heuristic fails
+    // developers are expected to subclass this provider and explicitly assert the connection.
+    File walFile = new File(databaseFile.getParent(), databaseFile.getName() + "-wal");
+    if (walFile.exists()) {
+      flags |= SQLiteDatabaseCompat.ENABLE_WRITE_AHEAD_LOGGING;
+    }
+
+    return flags;
+  }
+
+  /**
+   * Perform the open per the options provided in {@link #determineOpenOptions(File)}.
+   * Subclassing is supported however this typically indicates a missing feature of some kind
+   * in {@link SQLiteDatabaseCompat} that should be patched in Stetho itself.
+   */
+  protected SQLiteDatabase performOpen(File databaseFile, @SQLiteOpenOptions int options) {
+    int flags = SQLiteDatabase.OPEN_READWRITE;
+
+    SQLiteDatabaseCompat compatInstance = SQLiteDatabaseCompat.getInstance();
+    flags |= compatInstance.provideOpenFlags(options);
+
+    SQLiteDatabase db = SQLiteDatabase.openDatabase(
         databaseFile.getAbsolutePath(),
         null /* cursorFactory */,
-        SQLiteDatabase.OPEN_READWRITE);
+        flags);
+    compatInstance.enableFeatures(options, db);
+    return db;
   }
 }
